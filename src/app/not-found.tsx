ac2bbd72a04674b090428166e2f5cff8cc1f3d70
Ollan Muza <ammmuza@gmail.com>
2025-10-18 17:19:33 +0100

    Refactor 404 error handling: consolidate styles and component into NotFoundPage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,18 @@
-import NotFound from "@/components/error/NotFound";
+import { IconZoomExclamation } from "@tabler/icons-react";
+import Link from "next/link";
+
 export default function NotFoundPage() {
-  return <NotFound />;
+  return (
+    <div className="">
+      <div className="error-404">
+        <IconZoomExclamation size={100} />
+        <h1>404 Page not found</h1>
+        <p>
+          Lost in the digital wilderness? This page {"couldn't"} find its way
+          either.
+        </p>
+        <Link href="/">Return Home</Link>
+      </div>
+    </div>
+  );
 }
